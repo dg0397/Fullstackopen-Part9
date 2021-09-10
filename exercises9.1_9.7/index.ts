b1b7c9c90f5858ee3dd7f475b3833906200a9174
dg0397 <rgg0397@gmail.com>
2021-09-10 00:50:02 -0400

    9.4 Express

---
+++ exercises9.1_9.7/index.ts
@@ -0,0 +1,11 @@
+import express = require('express');
+
+const app = express()
+
+const PORT = 3000
+
+app.get('/hello',(_req,res)=>{
+    res.send('Hello Full Stack!')
+})
+
+app.listen(PORT,() => console.log(`Server running on port ${PORT}`))
