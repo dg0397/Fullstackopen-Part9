5e6e6f2e2eaf7a0fcd5b7b205f68db909405d2f9
dg0397 <rgg0397@gmail.com>
2021-09-15 11:02:32 -0400

    9.10: Patientor backend, step3

---
+++ patientor_backend/src/services/diagnoseService.ts
@@ -0,0 +1,12 @@
+import diagnoses from '../../data/diagnoses';
+
+import { DiagnoseEntry } from '../types';
+
+const getEntries = () : Array<DiagnoseEntry> => {
+    return diagnoses;
+};
+
+export default {
+    getEntries
+};
+
