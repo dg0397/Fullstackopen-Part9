4a14b68a959308f307da8b66c9eede11f7716c02
dg0397 <rgg0397@gmail.com>
2021-09-19 09:14:16 -0400

    9.15.

--- course-information-with-typescript/src/App.tsx
+++ course-information-with-typescript/src/App.tsx
@@ -1,16 +1,135 @@
 import React from 'react';
 
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartWithDescription extends CoursePartBase {
+  description : string
+}
+
+interface CourseNormalPart extends CoursePartWithDescription {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartWithDescription {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartWithDescription {
+  type: "special";
+  requirements : Array<string>
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
+// this is the new coursePart variable
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is the leisured course part",
+    type: "normal"
+  },
+  {
+    name: "Advanced",
+    exerciseCount: 7,
+    description: "This is the harded course part",
+    type: "normal"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: "groupProject"
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+    type: "submission"
+  },
+  {
+    name: "Backend development",
+    exerciseCount: 21,
+    description: "Typing the backend",
+    requirements: ["nodejs", "jest"],
+    type: "special"
+  }
+]
+
 interface HeaderProps {
   name:string
 }
 
-interface ContentPropsTypes {
-  name : string;
-  exerciseCount : number
-} 
-
 interface DataProps {
-  data : Array<ContentPropsTypes>
+  data : Array<CoursePart>
+}
+
+const assertNever = (value:never) :never =>{
+  throw new Error(
+    `Unhandled discriminated union menber: ${JSON.stringify(value)}`
+  );
+};
+
+
+const Part = ({data}:DataProps) => {
+  return(
+    <>
+      {
+        data.map( part => {
+          switch (part.type) {
+            case 'normal' : {
+              return (
+                <div>
+                  <h3>{part.name} {part.exerciseCount}</h3>
+                  <em>{part.description}</em>
+                </div>
+              )
+            }
+            case 'groupProject' : {
+              return (
+                <div>
+                  <h3>{part.name} {part.exerciseCount}</h3>
+                  <p>project exercises {part.groupProjectCount}</p>
+                </div>
+              )
+            }
+            case 'submission' : {
+              return (
+                <div>
+                  <h3>{part.name} {part.exerciseCount}</h3>
+                  <em>{part.description}</em>
+                  <p>submit to {part.exerciseSubmissionLink}</p>
+                </div>
+              )
+            }
+            case 'special': {
+              return (
+                <div>
+                  <h3>{part.name} {part.exerciseCount}</h3>
+                  <em>{part.description}</em>
+                  <p>required skils {part.requirements.join(', ')}</p>
+                </div>
+              )
+            }
+            default:{
+              return assertNever(part)
+            }
+          }
+        })
+      }
+    </>
+  )
 }
 const Header = ({name}:HeaderProps):JSX.Element => {
   return (
@@ -21,13 +140,7 @@ const Header = ({name}:HeaderProps):JSX.Element => {
 const Content = ({data:content}:DataProps):JSX.Element => {
   return (
     <>
-      {
-        content.map(({name,exerciseCount}) => {
-          return(
-            <p key = {name}>{name} {exerciseCount}</p>
-          )
-        })
-      }
+      <Part data = {content}/>
     </>
   )
 };
@@ -41,20 +154,6 @@ const Total = ({data}:DataProps) : JSX.Element => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
   return (
     <div>
       <Header name={courseName} />
