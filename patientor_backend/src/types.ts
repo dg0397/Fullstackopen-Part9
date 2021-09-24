5fdb98c775d949aaab807eaccd3354babd68484a
dg0397 <rgg0397@gmail.com>
2021-09-24 08:36:13 -0400

    9.19: patientor, step4

--- patientor_backend/src/types.ts
+++ patientor_backend/src/types.ts
@@ -27,10 +27,53 @@ export type Fields = {
     gender : unknown,
     occupation : unknown
 };
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
 
+type BaseEntry = {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<DiagnoseEntry['code']>;
+    type: string;
+};
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
 }
+
+type DischargeType = {
+    date: string,
+    criteria: string
+};
+
+type SickLeave = {
+    startDate: string;
+    endDate: string
+};
+
+interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: DischargeType;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeave; 
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type NewPatientEntry = Omit<PatientEntry, 'id' | 'entries'>;
 
